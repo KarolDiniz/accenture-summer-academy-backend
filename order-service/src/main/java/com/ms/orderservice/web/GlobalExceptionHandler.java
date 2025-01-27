8e2bd6d44727afbbdfbb008b0ea37c2d0e689d26
Ellen Cassia Matos <ellencassiamatos@gmail.com>
2025-01-27 11:40:39 -0300

    Mudação no GlobalExceptionHandler

--- order-service/src/main/java/com/ms/orderservice/web/GlobalExceptionHandler.java
+++ order-service/src/main/java/com/ms/orderservice/web/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.ms.orderservice.web;
 
-
-import com.ms.orderservice.model.exception.InvalidStatusTransitionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
