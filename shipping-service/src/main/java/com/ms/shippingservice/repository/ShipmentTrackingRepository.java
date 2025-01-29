57aa9f2c8adc2a4d76495abe6a37182f8cf29d76
Karoline Diniz Ramos <dinizkaroline923@gmail.com>
2025-01-29 14:39:19 -0300

    :recycle: refactor: remove shipping-service

--- shipping-service/src/main/java/com/ms/shippingservice/repository/ShipmentTrackingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ms.shippingservice.repository;
-
-import com.ms.shippingservice.model.ShipmentTracking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ShipmentTrackingRepository extends JpaRepository<ShipmentTracking, Long> {
-    List<ShipmentTracking> findByShippingOrderIdOrderByTimestampDesc(Long shippingOrderId);
-}
